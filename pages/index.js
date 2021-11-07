d3b597e091d85dbddb62966392cef36c142e2d21
Christian Farris <christianfarriscs@gmail.com>
2021-11-06 23:23:13 -0500

    feat: display cryptos in cards on homepage from coinbase

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,28 @@
-import { $fetch } from 'ohmyfetch'
+import NextAuth from '../components/NextAuth'
+import { getSupportedCryptos } from '../server/services/cryptos-service'
 
-const Home = ({ priceBTC }) => {
+const Home = ({ cryptos }) => {
   return (
     <div>
-      <strong>Bitcoin</strong>
-      <p>{priceBTC}</p>
+      <NextAuth />
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '1rem', width: '768px', flexWrap: 'wrap', margin: '0 auto' }}>
+        {cryptos.map(({ id, name, value }) => (
+          <div key={id} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', margin: '1rem', padding: '1rem', border: '1px solid #000', borderRadius: '.25rem', width: '256px' }}>
+            <div style={{ display: 'flex' }}>
+              <p>{id} - {name}</p>
+            </div>
+            <p>{value}</p>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
 
 export const getServerSideProps = async () => {
-  const {data: priceBTC} = await $fetch('http://localhost:3000/api/coins/btc')
-
   return {
     props: {
-      priceBTC
+      cryptos: await getSupportedCryptos()
     }
   }
 }
