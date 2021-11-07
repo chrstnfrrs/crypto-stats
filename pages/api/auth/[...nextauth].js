896a772e7009b18f131e4b5d95924975458fa359
Christian Farris <christianfarriscs@gmail.com>
2021-11-06 23:24:50 -0500

    feat: configure coinbase provider for OAuth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,28 +1,48 @@
-import NextAuth from "next-auth"
-import CoinbaseProvider from "next-auth/providers/coinbase"
+import NextAuth from 'next-auth';
+import CoinbaseProvider from 'next-auth/providers/coinbase';
+import { createCoinbaseLink } from '../../../server/utils/coinbase-link-utils';
 
 export default NextAuth({
-  // Configure one or more authentication providers
   callbacks: {
-    async signIn(params) {
-      console.log('params', params)
-      return true
+    async jwt({ token, account }) {
+      // Persist the OAuth access_token to the token right after signin
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
     },
-    async redirect({ url, baseUrl }) {
-      return baseUrl
+    async session({ session, token, user }) {
+      // Send properties to the client, like an access_token from a provider.
+      session.accessToken = token.accessToken;
+      session.accountId = token.accountId;
+      return session;
     },
-    async session({ session, user, token }) {
-      return session
+    redirect({ url, baseUrl }) {
+      const isAuthRoute = ['admin'].some((route) => url.endsWith(route));
+
+      if (isAuthRoute) {
+        return baseUrl;
+      }
+
+      return url.startsWith(baseUrl) ? url : baseUrl;
     },
-    async jwt({ token, user, account, profile, isNewUser }) {
-      return token
-    }
+    async signIn({ user, credentials, account, profile }) {
+      return true;
+    },
   },
   providers: [
     CoinbaseProvider({
+      authorization: createCoinbaseLink(),
       clientId: process.env.COINBASE_CLIENT_ID,
-      clientSecret: process.env.COINBASE_CLIENT_SECRET
+      clientSecret: process.env.COINBASE_CLIENT_SECRET,
     }),
-    // ...add more providers here
   ],
-})
+  jwt: {
+    secret: process.env.JWT_SECRET,
+  },
+  session: {
+    jwt: true,
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+    updateAge: 24 * 60 * 60, // 24 hours
+  },
+});
